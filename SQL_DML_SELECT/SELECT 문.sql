9033b37c1be1b6d88a3b8e46c17654341712fb04
chris <bound654@naver.com>
2024-06-13 09:36:32 +0900

    복습

--- SQL_DML_SELECT/SELECT 문.sql
+++ SQL_DML_SELECT/SELECT 문.sql
@@ -2,6 +2,6 @@
 -- , 기준으로 실행됨
 -- 커서를 둔 위치에서 ctrl + enter
 
--- 연습용 HR계정을 실행, 접속할 수 있음
+-- 연습용 HR계정을 실행, 데이터 접속할 수 있음
 
-ALTER USER HR IDENTIFIED BY HR ACCOUNT UNLOCK;
+ALTER USER HR IDENTIFIED BY HR ACCOUNT UNLOCK; -- 이거 처서 접속
